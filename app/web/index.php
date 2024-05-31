41d8a2c17d3c4799bc62e45387d0b7afa39eadee
Nick <kola9876543210@mail.ru>
2024-05-31 10:10:07 +0300

    feat: Добавить книги в карусели

--- app/web/index.php
+++ app/web/index.php
@@ -1,6 +1,10 @@
 <?php require($_SERVER['DOCUMENT_ROOT'] . '/php/template/elements/main/header.php'); ?>
 <!-- Подключаем заголовок -->
 
+<?php
+    require_once($_SERVER['DOCUMENT_ROOT'] . '/php/function/ConnectionDB.php');
+    $db = new ConnectionDB(); 
+?>
 <body>
     <!-- Подключаем header -->
     <?php require($_SERVER['DOCUMENT_ROOT'] . '/php/template/elements/el_header.php'); ?>
@@ -10,25 +14,29 @@
     <div>
         <!-- <hr> -->
         <!-- <p>Вам может быть интересно</p> -->
-        <?php /*тут какая-то переменная определяющая поведение карусели*/ 
-            $name_razdela_slider = "Вам может быть интересно";
-        ?>
-        <?php require($_SERVER['DOCUMENT_ROOT'] . '/php/template/elements/sliders/little_slider.php'); ?>
+    <?php /*тут какая-то переменная определяющая поведение карусели*/ 
+        $name_razdela_slider = "Вам может быть интересно";
+        $resources = $db->getRandomBooks(10);
+        require($_SERVER['DOCUMENT_ROOT'] . '/php/template/elements/sliders/little_slider.php');
+    ?>
     </div>
     <div>
         <hr>
         <p>Популярные категории</p>
         <!-- это тоже делается через бд, и через foreach, но ниже пока пусть будут приведены 2 элемента-->
-        <?php require($_SERVER['DOCUMENT_ROOT'] . '/php/template/elements/covers/one_category.php'); ?>
-        <?php require($_SERVER['DOCUMENT_ROOT'] . '/php/template/elements/covers/one_category.php'); ?>
+    <?php
+        require($_SERVER['DOCUMENT_ROOT'] . '/php/template/elements/covers/one_category.php');
+        require($_SERVER['DOCUMENT_ROOT'] . '/php/template/elements/covers/one_category.php');
+    ?>
     </div>
     <div>
         <!-- <hr> -->
         <!-- <p>Последнии добавления</p> -->
-        <?php /*тут какая-то переменная определяющая поведение карусели*/ 
-            $name_razdela_slider = "Последнии добавления";
-        ?>
-        <?php require($_SERVER['DOCUMENT_ROOT'] . '/php/template/elements/sliders/little_slider.php'); ?>
+    <?php /*тут какая-то переменная определяющая поведение карусели*/ 
+        $name_razdela_slider = "Последнии добавления";
+        $resources = $db->getRandomBooks(16);
+        require($_SERVER['DOCUMENT_ROOT'] . '/php/template/elements/sliders/little_slider.php');
+    ?>
     </div>
 
 
